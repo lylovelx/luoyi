a9739f22fa9c18cede392ae5010e1b7db60bf0bf
luoyiplusplus <13669181392@163.com>
2019-05-30 18:02:46 +0800

    #include<string> #include<iostream> using namespace std; string addStrings(string num1, string num2) { 	int l1 = num1.size(); 	int l2 = num2.size(); 	int flag = 0; 	if (l1>l2){ 		num2.insert(0, l1 - l2, '0'); 	} 	else{ 		num1.insert(0, l2 - l1, '0'); 	} 	int i = num1.size() - 1; 	while (i >= 0){ 		if ((num1[i] - '0') + (num2[i] - '0') + flag>9){ 			num1[i] = ((num1[i] - '0') + (num2[i] - '0') + flag) % 10 + '0'; 			flag = 1; 		} 		else{ 			num1[i] = (num1[i] - '0') + (num2[i] - '0') + flag + '0'; 			flag = 0; 		} 		i--; 	} 	if (flag == 1){ 		num1.insert(0, 1, '1'); 	} 	return num1; } int main(){ 	string a("9"); 	string b("99"); 	string ret = addStrings(a,b); 	cout << ret << endl; 	system("pause"); 	return 0; } #include<iostream> #include<string> using namespace std; int main(){ 	string str; 	cin >> str; 	int score = 0; 	//密码长度 	if (str.size() <= 4){ 		score += 5; 	} 	else if (str.size() >= 5 && str.size() <= 7){ 		score += 10; 	} 	else{ 		score += 25; 	} 	//字母 	int a = 0; 	int A = 0; 	for (int i = 0; i<str.size(); ++i){ 		if (str[i] >= 'a'&&str[i] <= 'z'){ 			a++; 		} 		if (str[i] >= 'A'&&str[i] <= 'Z'){ 			A++; 		} 	} 	if (a == str.size() || A == str.size()){ 		score += 10; 	} 	else if (a == 0 && A == 0){ 		score += 0; 	} 	else if (a>0 && A>0){ 		score += 20; 	} 	//数字 	int num = 0; 	for (int i = 0; i<str.size(); ++i){ 		if (str[i] >= '0'&&str[i] <= '9'){ 			num++; 		} 	} 	if (num == 0){ 		score += 0; 	} 	else if (num == 1){ 		score += 10; 	} 	else{ 		score += 20; 	} 	//符号 	int symbol = 0; 	for (int i = 0; i<str.size(); ++i){ 		if ((str[i] >= 0x21 && str[i] <= 0x2F) || (str[i] >= 0x3A && str[i] <= 0x40) || (str[i] >= 0x5B && str[i] <= 0x60) || (str[i] >= 0x7B && str[i] <= 0x7E)){ 			symbol++; 		} 	} 	if (symbol == 0){ 		score += 0; 	} 	else if (symbol == 1){ 		score += 10; 	} 	else{ 		score += 25; 	} 	//奖励 	if (num != 0 && a != 0 && A != 0 && symbol != 0){ 		score += 5; 	} 	else if (num != 0 && (a + A) != 0 && symbol != 0){ 		score += 3; 	} 	else if (num != 0 && (a + A) != 0 && symbol == 0){ 		score += 2; 	} 	if (score >= 90){ 		cout << "VERY_SECURE" << endl; 	} 	else if (score >= 80){ 		cout << "SECURE" << endl; 	} 	else if (score >= 70){ 		cout << "VERY_STRONG" << endl; 	} 	else if (score >= 60){ 		cout << "STRONG" << endl; 	} 	else if (score >= 50){ 		cout << "AVERAGE" << endl; 	} 	else if (score >= 25){ 		cout << "WEAK" << endl; 	} 	else{ 		cout << "VERY_WEAK" << endl; 	} 	system("pause"); 	return 0; } #include<iostream> #include<vector> #include<string> using namespace std; class Board { public: 	bool checkWon(vector<vector<int> > board) { 		int row = board.size(); 		int col = board[0].size(); 		int sum_row = 0; 		int sum_col = 0; 		for (int i = 0; i<row; ++i){ 			for (int j = 0; j<col; ++j){ 				sum_row += board[i][j]; 				sum_col += board[j][i]; 			} 			if (sum_row == 3 || sum_col == 3){ 				return true; 			} 		} 		int first = 0; 		int two = 0; 		for (int i = 0; i<row; ++i){ 			first += board[i][i]; 			two += board[i][row - 1 - i]; 		} 		if (first == 3 || two == 3){ 			return true; 		}
    		return false;
    	}
    };
    int dp(int n, int m){
    	int dp[n + 1][m + 1];
    	for (int i = 1; i <= n; ++i){
    		dp[i][0] = 1;
    	}
    	for (int j = 1; j <= m; ++j){
    		dp[0][j] = 1;
    	}
    	for (int i = 1; i <= n; ++i){
    		for (int j = 1; j <= m; ++j){
    			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
    		}
    	}
    	return dp[n][m];
    }
    int main(){
    	int n, m;
    	cin >> n >> m;
    	int sum = dp(n, m);
    	cout << sum << endl;
    	return 0;
    }

--- Project105/test.c
+++ Project105/test.c
@@ -1,101 +1,25 @@
-//#include<iostream>
-//#include<string>
-//#include<vector>
-//#include<stack>
-//using namespace std;
-//int main(){
-//	int n;
-//	cin >> n;
-//	string str[n];
-//	vector<int> v(n);
-//	for (int i = 0; i < n; ++i){
-//		cin >> str[i];
-//	}
-//	// «∑ÒŒ™◊÷µ‰–Ú≈≈¡–
-//	int flag1 = 1;
-//	int flag2 = 1;
-//	for (int i = 0; i < n; ++i){
-//		if (str[i]>str[i + 1]){
-//			flag1 = 0;
-//		}
-//	}
-//	for (int i = 0; i < n; ++i){
-//		v[i] = str[i].size();
-//	}
-//	for (int i = 0; i < n; ++i){
-//		if (v[i]>v[i + 1]){
-//			flag2 = 0;
-//		}
-//	}
-//	if (flag1 == 1 && flag2 != 1){
-//		cout << "lexicographically" << endl;
-//	}
-//	else if (flag1 == 1 && flag2 == 1){
-//		cout << "both" << endl;
-//	}
-//	else if (flag1 != 1 && flag2 == 1){
-//		cout << "lengths" << endl;
-//	}
-//	else{
-//		cout << "none" << endl;
-//	}
-//
-//	system("pause");
-//	return 0;
-//	
-//}
-//class Parenthesis {
-//public:
-//	bool chkParenthesis(string A, int n) {
-//		stack<char> s;
-//		for (auto e : A){
-//			switch (e){
-//			case '(':
-//			{
-//						s.push(e);
-//						break;
-//			}
-//			case ')':
-//			{
-//
-//						if (s.empty() || s.top() != '('){
-//							return false;
-//						}
-//						else{
-//							s.pop();
-//						}
-//						break;
-//			}
-//			default:
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-//};
-//#include<iostream>
-#include<stdio.h>
-//#include<algorithm>
-//using namespace std;
-int main(){
-	int a[1] = {1};
-	printf("%d",a[2]);
-	
-	/*int n, f, f0 = 0, f1 = 1, l = 0, r = 0;
-	cin >> n;
-	while (1){
-		f = f0 + f1;
-		f0 = f1;
-		f1 = f;
-		if (f<n){
-			l = n - f;
-		}
-		else{
-			r = f - n;
-			break;
+#include<iostream>
+
+using namespace std;
+class UnusualAdd {
+public:
+	UnusualAdd(int n=0){
+		if (i<n){
+			i++;
 		}
 	}
-	cout << min(l, r) << endl;*/
+	int addAB(int A, int B) {
+		UnusualAdd ret(A);
+		return i;
+	}
+private:
+	static int i;
+};
+int UnusualAdd::i = 0;
+int main(){
+	UnusualAdd a;
+	a.addAB(2, 3);
+	cout << UnusualAdd::i << endl;
 	system("pause");
 	return 0;
 }
