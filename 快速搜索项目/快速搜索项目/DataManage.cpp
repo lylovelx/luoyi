e28cfb4262b13567936c2460cdd96e0d1bbb6e81
13669181392@163.com <13669181392@163.com>
2019-07-23 23:09:15 +0800

    //class Solution { //public: //	/** //	 *  奇数位上都是奇数或者偶数位上都是偶数 //	 *  输入：数组arr，长度大于2 //	 *  len：arr的长度 //	 *  将arr调整成奇数位上都是奇数或者偶数位上都是偶数 //	 */ //	void oddInOddEvenInEven(vector<int>& arr, int len) { //		long i = 0, j = 1; //		while (i < len&&j < len) { //			if (arr[i] % 2 == 0) { //				i += 2; //				continue; //			} //			if (arr[j] % 2 != 0) { //				j += 2; //				continue; //			} //			swap(arr[i], arr[j]); //		} //		return; //	} //}; // #include<iostream> #include<cmath> using namespace std; int main() { 	int n; 	while (cin >> n) { 		if (n == 0) 			break; 		cout << (long long)pow(5, n) - 4 << " " << (long long)pow(4, n) + n - 4 << endl; 	} 	system("pause"); 	return 0; }

--- 快速搜索项目/快速搜索项目/DataManage.cpp
+++ 快速搜索项目/快速搜索项目/DataManage.cpp
@@ -1,5 +1,4 @@
 #include"DataManage.h"
-#define _CRT_SECURE_NO_WARNINGS
 void SqliteManager::Open(const string& path) {
 
 	int ret=sqlite3_open(path.c_str(), &_db);
@@ -16,36 +15,58 @@ void SqliteManager::ExecuteSql(const string& sql) {
 void SqliteManager::GetTable(const string& sql, int& row, int& col, char**& ppRet) {
 	int ret = sqlite3_get_table(_db,sql.c_str(),&ppRet,&row,&col,&errmsg);
 	if (ret!=SQLITE_OK) {
-		assert(false);
+		//assert(false);
 	}
 }
+void DataManager::Init() {
+	char sql[256];
+	sprintf(sql, "create table %s (id integer primary key autoincrement,doc_path text,doc_name text)",TB_DOC);
+	_dbmgr.ExecuteSql(sql);
 
+}
 void DataManager::GetDoc(const string& path, set<string>& doc) {
 	char query_sql[256];
-	sprintf(query_sql, "select doc_name from %s where doc_path = '%s'","tb_doc",path.c_str());
+	sprintf(query_sql, "select doc_name from %s where doc_path = '%s'",TB_DOC,path.c_str());
 	int row, col;
 	char** ppRet;
 	AutoGetTable get(&_dbmgr,query_sql,row,col,ppRet);
-	for (int i = 1; i < row;++i) {
+	for (int i = 1; i <= row;++i) {
 		doc.insert(ppRet[i]);
 	}
 }
-
+//void SqliteManager::Get(sqlite3*& db) {
+//	db = _db;
+//}
+//void DataManager::GetInstance(sqlite3*& db) {
+//	_dbmgr.Get(db);
+//}
 void DataManager::InsertDoc(const string& path, const string& doc) {
 
 	char sql[256];
-	sprintf(sql,"insert into %s values('%s','%s')","tb_doc",path.c_str(),doc.c_str());
+	sprintf(sql,"insert into %s(doc_path,doc_name) values('%s','%s')",TB_DOC,path.c_str(),doc.c_str());
 	_dbmgr.ExecuteSql(sql);
 }
 void DataManager::DeleteDoc(const string& path,const string& doc) {
 	char sql[256];
-	sprintf(sql,"delete from %s where doc_path = '%s' and doc_name = '%s'","tb_doc",path.c_str(),doc.c_str());
+	sprintf(sql,"delete from %s where doc_path = '%s' and doc_name = '%s'",TB_DOC,path.c_str(),doc.c_str());
 	_dbmgr.ExecuteSql(sql);
 
+	//char sql_s[256];
 	string path_ = path;
 	path_ += '\\';
 	path_ += doc;
-	sprintf(sql,"delete from %s where doc_path like '%s%'","tb_doc",path.c_str());
+	sprintf(sql,"delete from %s where doc_path like '%s%%'",TB_DOC,path_.c_str());
 	_dbmgr.ExecuteSql(sql);
-	
+}
+void DataManager::SearchDoc(const string& key, vector<pair<string,string>>& docs) {
+	//利用模糊匹配搜索数据并打印
+	char sql[256];
+	//sqlite中文模糊匹配不支持模糊查询，只支持精确查询
+	sprintf(sql, "select doc_path,doc_name from %s where doc_name like '%%%s%%'", TB_DOC,key.c_str());
+	int row, col;
+	char** ppRet;
+	AutoGetTable get(&_dbmgr,sql,row,col,ppRet);
+	for (int i = 1; i <= row;++i) {
+		docs.push_back(make_pair(ppRet[i*col+0],ppRet[i*col+1]));
+	}
 }
